d02d9c0a318d0edc41f8b927e38491bdb39146b2
Mohanad Hosam <nodyez14@gmail.com>
2021-02-25 13:25:26 +0300

    Serve an html document instead of a string

--- myApp.js
+++ myApp.js
@@ -6,7 +6,8 @@ var app = express();
 
 print("Hello World");
 
-app.get("/", (_, res) => res.send("Hello Express"));
+// app.get("/", (_, res) => res.send("Hello Express"));
+app.get("/", (_, res) => res.sendFile(`${__dirname}views/index.html`));
 
 
 
