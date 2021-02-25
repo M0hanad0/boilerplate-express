e1c13a51888bab101e86b192b80cc0b155e74d7d
Mohanad Hosam <nodyez14@gmail.com>
2021-02-25 13:44:08 +0300

    Move static mounting to the top

--- myApp.js
+++ myApp.js
@@ -4,11 +4,11 @@ const print = log;
 var express = require('express');
 var app = express();
 
+app.use(express.static(`${__dirname}/public`))
+
 print("Hello World");
 // app.get("/", (_, res) => res.send("Hello Express"));
-app.use(express.static(`${__dirname}/public`))
 app.get("/", (_, res) => res.sendFile(`${__dirname}/views/index.html`));
-
 
 
 
