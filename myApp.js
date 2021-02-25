f537bc19bd823a7c3638647443926e886a7559d8
Mohanad Hosam <nodyez14@gmail.com>
2021-02-25 14:00:21 +0300

    Add /public path to app.use

--- myApp.js
+++ myApp.js
@@ -5,8 +5,8 @@ const express = require('express');
 const app = express();
 
 // app.use(express.static(`${__dirname}/public`));
-app.use(express.static(__dirname + "/public"));
-app.use("/assets", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+// app.use("/assets", express.static(__dirname + "/public"));
 
 print("Hello World");
 // app.get("/", (_, res) => res.send("Hello Express"));
